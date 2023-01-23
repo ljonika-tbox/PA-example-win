2ea90d61840b1ba3b29f4c77332ad16a036a8e88
Linas Jonika <linas.jonika1@gmail.com>
2023-01-24 00:05:21 +0200

    first commiy

---
+++ windows/Predictable_7/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Predictable_7::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Predictable_7::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
